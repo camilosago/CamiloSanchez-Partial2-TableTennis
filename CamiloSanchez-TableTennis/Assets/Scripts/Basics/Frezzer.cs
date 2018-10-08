bf569b79d96926bfb8d5a80292ed7b5bd8ed7361
Camilo Sanchez <cami2sanchez@gmail.com>
2018-10-08 18:43:10 -0500

    hola

---
+++ CamiloSanchez-TableTennis/Assets/Scripts/Basics/Frezzer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frezzer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.up * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.down * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.back * 0.3f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.forward * 0.3f);
+        }
+    }
+}
